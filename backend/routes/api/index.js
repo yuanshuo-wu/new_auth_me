170fdc4f7c2cddde7c8ea086fad08d9d2d795a97
yuanshuo_wu <yuanshuowu200502@gmail.com>
2023-02-23 02:33:16 -0800

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
 // });
