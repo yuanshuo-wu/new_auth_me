3c2eacfe60aba32297ea15fd7a00984c572c5f77
yuanshuo_wu <yuanshuowu200502@gmail.com>
2023-02-24 16:48:41 -0800

    seeder bug fixed

--- backend/db/migrations/20230224084158-create-spots.js
+++ backend/db/migrations/20230224084158-create-spots.js
@@ -17,7 +17,7 @@ module.exports = {
       },
       ownerId: {
         type: Sequelize.INTEGER,
-        references: { model: 'User' },
+        references: { model: 'Users' },
         //onDelete: 'CASCADE'????
       },
       address: {
